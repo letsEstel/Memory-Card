10b1e0b582f43689d36b1e4084086bafb9a85b3e
Estel <jiankangzhao7@gmail.com>
2024-12-14 23:59:18 +0800

    Compelete the intial game

--- memory-card/src/components/GameBoard.jsx
+++ memory-card/src/components/GameBoard.jsx
@@ -1,11 +1,73 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import GameCards from "./GameCards";
 export default function GameBoard() {
   const [score, setScore] = useState(0);
   const [card, setCard] = useState([]);
-  return (
-    <>
-      <h1>Score {score}</h1>
-    </>
-  );
+  const [highest, setHighest] = useState(0);
+  const [load, setLoad] = useState(true);
+  useEffect(() => {
+    async function loadCards() {
+      const cardsData = await GameCards(9);
+      setCard(cardsData);
+      setLoad(false);
+    }
+    loadCards();
+  }, []);
+
+  /* Randomize array in-place using Durstenfeld shuffle algorithm */
+  function shuffleArray(array) {
+    for (var i = array.length - 1; i >= 0; i--) {
+      var j = Math.floor(Math.random() * (i + 1));
+      var temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+    }
+  }
+
+  function clickPic(pic) {
+    if (pic[1] == 0) {
+      setScore(score + 1);
+      if (score >= highest) {
+        setHighest(score + 1);
+      }
+      setCard(
+        card.map((car) => {
+          if (car == pic) {
+            car[1] = 1;
+            return car;
+          } else return car;
+        })
+      );
+      let newCard = card;
+      shuffleArray(newCard);
+      setCard(newCard);
+    } else {
+      setScore(0);
+      setCard(
+        card.map((car) => {
+          return [car[0], 0];
+        })
+      );
+    }
+  }
+  if (load) return <h1>Loading</h1>;
+  else {
+    return (
+      <>
+        <h1>Score {score}</h1>
+        <h1>Highest {highest}</h1>
+        <ul>
+          {card.map((pic) => (
+            <li key={pic[0]}>
+              <img
+                src={`${pic[0]}`}
+                alt="Game Card"
+                onClick={() => clickPic(pic)}
+              />
+            </li>
+          ))}
+        </ul>
+      </>
+    );
+  }
 }
